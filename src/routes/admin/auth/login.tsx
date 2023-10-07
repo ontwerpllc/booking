36feb16b5ce8e86a9076306425f07dd063b5d224
Wesley Jochman <wjochman@gmail.com>
2023-10-07 10:35:21 -0500

    Fix ant design with tailwind

--- src/routes/admin/auth/login.tsx
+++ src/routes/admin/auth/login.tsx
@@ -1,5 +1,72 @@
+import { Button, Form, Input } from 'antd';
+
+type FieldType = {
+  email?: string;
+  password?: string;
+};
+
 const AdminLogin = () => {
-  return <div>Admin Login</div>;
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <Form
+            initialValues={{ remember: true }}
+            onFinish={() => {}}
+            onFinishFailed={() => {}}
+            layout="vertical"
+          >
+            <Form.Item<FieldType>
+              label="Email"
+              name="email"
+              rules={[
+                { required: true, message: 'Please input your username' },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: 'Please input your password' },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" block>
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Start a 14 day free trial
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AdminLogin;
